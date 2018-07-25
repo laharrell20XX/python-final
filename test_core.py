1397f23d958ad6d6f01f61b15a5a1c8f7ff1188f
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-25 11:14:29 -0500

    added tests for return_item and rent_item

--- test_core.py
+++ test_core.py
@@ -65,3 +65,51 @@ def test_convert_inventory_full():
     assert core.convert_inventory(inventory_list) == [
         'car,2,3,4,5\n', 'rabbit,5,6,7,8\n', 'pen,9,10,11,12\n'
     ]
+
+
+def test_rent_item_instock():
+    item = {
+        'item_name': 'car',
+        'base_rental_price': 2,
+        'replacement_cost': 3,
+        'in_stock': 4,
+        'initial_stock': 5
+    }
+    core.rent_item(item)
+    assert item['in_stock'] == 3
+
+
+def test_rent_item_out_of_stock():
+    item = {
+        'item_name': 'car',
+        'base_rental_price': 2,
+        'replacement_cost': 3,
+        'in_stock': 0,
+        'initial_stock': 5
+    }
+    core.rent_item(item)
+    assert item['in_stock'] == -1
+
+
+def test_return_item_half_stocked():
+    item = {
+        'item_name': 'car',
+        'base_rental_price': 2,
+        'replacement_cost': 3,
+        'in_stock': 3,
+        'initial_stock': 6
+    }
+    core.return_item(item)
+    assert item['in_stock'] == 4
+
+
+def test_return_item_full_stocked():
+    item = {
+        'item_name': 'car',
+        'base_rental_price': 2,
+        'replacement_cost': 3,
+        'in_stock': 6,
+        'initial_stock': 6
+    }
+    core.return_item(item)
+    assert item['in_stock'] == 7
