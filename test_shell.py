24ee19bf9142e48387617d6c08637aaefd9ad87f
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-25 09:27:48 -0500

    added tests for rent_or_return

--- test_shell.py
+++ test_shell.py
@@ -25,3 +25,31 @@ def test_employee_or_customer_cust(output):
 The identity you entered is not valid.  Please try again
 Are you an [e]mployee or a [c]ustomer? (type 'leave' to exit the application)
 >>> C'''
+
+
+@with_inputs('q', 'eturn', 'Return')
+@should_print
+def test_rent_or_return_return(output):
+    assert shell.rent_or_return() == 'return'
+    assert output == '''Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> q
+Invalid option
+Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> eturn
+Invalid option
+Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> Return'''
+
+
+@with_inputs('q', 'ent', 'RENT')
+@should_print
+def test_rent_or_return_rent(output):
+    assert shell.rent_or_return() == 'rent'
+    assert output == '''Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> q
+Invalid option
+Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> ent
+Invalid option
+Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+>>> RENT'''
