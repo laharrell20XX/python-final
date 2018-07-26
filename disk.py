f79389cc5f257cd2edc1f35342fe672731fa1a24
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-26 17:01:12 -0500

    added function process_user_items

--- disk.py
+++ disk.py
@@ -30,16 +30,20 @@ def login(manifesto_file, username):
     already otherwise it creates a new user
     '''
     with open(manifesto_file) as file:
-        usernames = file.readlines()
-    if usernames:  #checks if there is something in the file
-        for name in usernames:
+        list_of_usernames = file.readlines()
+    if list_of_usernames:  #checks if there is something in the file
+        for name in list_of_usernames:
             if username == name.split(',')[0]:  #checks if the username exists
-                return True  #stops iterations if the name is found
+                return True, list_of_usernames  #stops iterations if the name is found
             new_user(manifesto_file, username)
-            return False
+            with open(manifesto_file) as file:
+                list_of_usernames = file.readlines()
+            return False, list_of_usernames
     else:  #first customer gets added since there are no other names
         new_user(manifesto_file, username)
-        return False
+        with open(manifesto_file) as file:
+            list_of_usernames = file.readlines()
+        return False, list_of_usernames
 
 
 def new_user(manifesto_file, username):
@@ -48,4 +52,24 @@ def new_user(manifesto_file, username):
     Adds the provided username to the manifesto_file
     '''
     with open(manifesto_file, 'a') as file:
-        file.write(f'{username},(item)\n')
+        file.write(f'{username}, (item)\n')
+
+
+def process_user_items(list_of_usernames):
+    ''' (list of str) -> dict
+
+    takes the user and any items that they have rented and turns it into a dictionary
+    '''
+    list_of_users = []
+    for user in list_of_usernames:
+        user = user.strip().split(', ')
+        user_dict = dict([user])
+        list_of_users.append(user_dict)
+    return list_of_users
+
+
+def rewrite_manifesto_file(manifesto_file, items):
+    ''' (file, list) -> file
+    
+    rewrites the manifesto_file with any changes that occurred after a transaction.
+    '''
