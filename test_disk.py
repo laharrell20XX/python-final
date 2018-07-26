51e8547716ae980b7a0efa5489d5f68e5a8d1250
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-26 15:05:05 -0500

    added tests for login and new_user

--- test_disk.py
+++ test_disk.py
@@ -28,3 +28,26 @@ def test_rewrite_inventory():
 rabbit,5,6,7,8
 pen,9,10,11,12
 '''
+
+
+@fake_file({'manifesto.txt': ''})
+def test_login_first_customer():
+    assert not disk.login('manifesto.txt', 'Logan')
+    assert open('manifesto.txt').read() == 'Logan,(item)\n'
+
+
+@fake_file({'manifesto.txt': 'Logan,(item)\nBill,(item)\n'})
+def test_login_new_customer():
+    assert not disk.login('manifesto.txt', 'Bob')
+    assert open('manifesto.txt').read() == '''Logan,(item)
+Bill,(item)
+Bob,(item)
+'''
+
+
+@fake_file({'manifesto.txt': 'Logan,(item)\nBill,(item)\n'})
+def test_login_existing_customer():
+    assert disk.login('manifesto.txt', 'Logan')
+    assert open('manifesto.txt').read() == '''Logan,(item)
+Bill,(item)
+'''
