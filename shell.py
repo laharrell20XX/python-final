23fe10929bbd31e25385d1126f44d4a69baf8b17
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-30 10:43:38 -0500

    ux and ui updated with inputs to leave areas of the program; the path of operations for the customer has been moved to its own function; history, inventory, and the customer manifesto can be updated; receipt is shown

--- shell.py
+++ shell.py
@@ -1,4 +1,6 @@
 import core, disk
+from os import system
+from time import sleep
 
 
 def greeting():
@@ -17,7 +19,7 @@ def show_inventory(inventory, identity):
     if identity == 'c':  #what customer sees
         for item in inventory:
             if item['in_stock']:
-                inventory_str += f'{item["item_name"]}: ${item["base_rental_price"]:.2f} to rent (${item["replacement_cost"]:.2f} to replace), {item["in_stock"]} left in stock\n'
+                inventory_str += f'\n{item["item_name"]}: ${item["base_rental_price"]:.2f} to rent (${item["replacement_cost"]:.2f} to replace), {item["in_stock"]} left in stock\n'
         print(inventory_str)
     if identity == 'e':  #what employee sees
         for item in inventory:
@@ -32,11 +34,16 @@ def rent_or_return():
     '''
     while True:
         choice = input(
-            "Hi!  Type 'rent' to rent an item, or type 'return' to return an item.\n>>> "
-        ).lower()
+            '''Hi!  Type 'rent' to rent an item, or type 'return' to return an item.
+Type 'checkout' when you want to checkout the items in your cart and type 'leave' to go back to the employee/customer screen.
+>>> ''').lower()
         if choice == 'rent':
             return choice
         elif choice == 'return':
+            return choice
+        elif choice == 'checkout':
+            return choice
+        elif choice == 'leave':
             return choice
         else:
             print('Invalid option')
@@ -59,7 +66,8 @@ def employee_or_customer():
             print('Have a nice day!')
             quit()
         else:
-            print("The identity you entered is not valid.  Please try again")
+            print(
+                "\nThe identity you entered is not valid.  Please try again\n")
 
 
 def user_login():
@@ -69,22 +77,27 @@ def user_login():
     a new user is created if they don't exist in the file
     '''
     while True:
-        username = input('Welcome! Please enter your name to continue.\n>>> ')
-        if username.strip():
-            if disk.login('customer_manifesto.txt', username):
-                print(f'Welcome back {username}!')
+        username = input(
+            'Welcome! Please enter your name to continue.\n>>> ').strip()
+        if username:
+            if username == 'back':
+                return username
+            elif disk.login('customer_manifesto.txt', username):
+                print(f'Welcome back {username}!\n\n')
                 return username
             else:
                 disk.new_user('customer_manifesto.txt', username)
-                print(f'Took you long enough to find us {username}! Welcome!')
+                print(
+                    f'Took you long enough to find us {username}! Welcome!\n\n'
+                )
                 return username
-        print('Sorry, you must enter a username')
+        print('Sorry, you must enter a username\n')
 
 
 def which_item(inventory, mode, customer, customer_manifesto):
-    ''' (str, str) -> dict
+    ''' (str, str) -> dict, str
 
-    Asks user which item they want and returns that item as a dictionary
+    Asks user which item they want and returns that item as a dictionary unless they type back
     '''
     while True:
         if mode == 'rent':
@@ -103,6 +116,8 @@ def rent_mode(inventory):
         item_choice = input(
             '\nType the name of the item you want to rent (case sensitive)\n>>> '
         )
+        if item_choice == 'back':
+            return item_choice
         for item in inventory:
             if item_choice == item['item_name'] and item['in_stock']:
                 return item
@@ -123,17 +138,21 @@ def return_mode(inventory, customer, customer_manifesto):
             print(f'You have {len(rented_items)} item(s) you have rented:')
             print('', *rented_items, sep='\n\t', end='\n')
             item_choice = input(
-                '\nType the name of the item you want to return (case sensitive)\n>>> '
-            )
+                '\nType the name of the item you want to return; Type "back" to return to the rent/return screen \n>>> '
+            ).lower()
+            if item_choice == 'back':
+                return item_choice
             for item in inventory:
-                if item_choice == item['item_name'] and (
-                        item_choice in rented_items):
-                    return item  #returns any item choice that is first entered
+                if item_choice == item['item_name'].lower() and (
+                        item_choice in [i.lower() for item in rented_items]):
+                    return item
             print('Sorry, that item has not been rented by you.\n')
         else:
             print(
-                "You haven't rented anything yet.  Redirecting you to the login screen..."
+                "You haven't rented anything yet.  Redirecting you to the rent or return screen...\n\n"
             )
+            sleep(3)
+            _ = system("clear")
             return None
 
 
@@ -142,43 +161,50 @@ def show_receipt(cart, customer):
 
     prints out the receipt to the user; negative number means money was refunded
     '''
-    grand_total = core.checkout(cart)
-    grand_tax = core.transaction_tax(cart)
-    print(f'\t{customer}')
-    print('Receipt'.center(30) + '\n')
+    grand_total = '$' + '{:.2f}'.format(core.checkout(cart))
+    grand_tax = '$' + '{:.2f}'.format(core.transaction_tax(cart))
+    print(f'{customer}')
+    print('Receipt'.center(40) + '\n')
     print('*' * 40)
     for item in cart:
         if 'rent' in item:
-            print(
-                '''\t{:<10}{:>10.2f}{:>8}
-\t{:<10}{:>18.2f}'''.format(item[0]['item_name'], item[0]['base_rental_cost'],
-                            item[1]), '(Replacement Cost)',
-                item[0]['replacement_cost'] * .1)
+            print('''\t{:<15}{:>10}{:>8}
+\t{:<10}{:>18.2f}'''.format(item[0]['item_name'], item[0]['base_rental_price'],
+                            item[1], '(Replacement Cost)',
+                            (item[0]['replacement_cost'] * .1)))
         if 'return' in item:
             print('''\t{:<10}{:>10.2f}{:>8}'''.format(
                 item[0]['item_name'], -item[0]['replacement_cost'] * .1,
                 item[1]))
     print('*' * 40)
-    print('''\t{:<10}{:>18.2f}
-\t{:<10}{:>18.2f}'''
+    print('''\t{:<10}{:>18}
+\t{:<10}{:>18}'''
           .format('Tax', grand_tax, 'Total',
                   grand_total))  #prints tax total then prints grand total
 
 
-def main():
-    greeting()
-    inventory = core.process_inventory(disk.read_inventory('inventory.txt'))
-    identity = employee_or_customer()
-    cart = []
+def customer_path(identity, inventory, cart):
+    ''' (str, list of dict, list of lists [dict, str]) -> NoneType
+
+    Options for the customer when they run the application 
+    '''
+    username = user_login()
     while True:
         if identity == 'c':  #customer path
-            username = user_login()
+            if username == 'back':
+                _ = system("clear")
+                break
             choice = rent_or_return()
             customer_manifesto = disk.process_user_items(
                 disk.read_manifesto('customer_manifesto.txt'))
+            if choice == 'leave':
+                _ = system("clear")
+                break
             if choice == 'rent':
                 item_choice = which_item(inventory, 'rent', username,
                                          customer_manifesto)
+                if item_choice == 'back':
+                    continue
                 core.rent_item(item_choice)
                 print(
                     f'1 {item_choice["item_name"]} has been added to your cart.'
@@ -187,14 +213,13 @@ def main():
             if choice == 'return':
                 item_choice = which_item(inventory, 'return', username,
                                          customer_manifesto)
-                if item_choice:
-                    core.return_item(item_choice)
-                    print(
-                        f'1 {item_choice["item_name"]} has been returned.  Please checkout to get your deposit back.'
-                    )
-                    core.add_item_to_cart(cart, item, choice)
-                else:
+                if item_choice == 'back':
                     continue
+                core.return_item(item_choice)
+                print(
+                    f'1 {item_choice["item_name"]} has been returned.  Please checkout to get your deposit back.'
+                )
+                core.add_item_to_cart(cart, item_choice, choice)
             if choice == 'checkout':  #next on the agenda
                 show_receipt(cart, username)
                 grand_total = core.checkout(cart)
@@ -204,7 +229,18 @@ def main():
                                             new_customer_manifesto)
                 disk.rewrite_inventory('inventory.txt', inventory)
                 disk.update_history('history.txt', username, grand_total)
-                print('\n\nGoodbye, have a nice day')
+                print('\n\nGoodbye, have a nice day!')
+                quit()
+
+
+def main():
+    greeting()
+    while True:
+        inventory = core.process_inventory(
+            disk.read_inventory('inventory.txt'))
+        identity = employee_or_customer()
+        cart = []
+        customer_path(identity, inventory, cart)
 
 
 if __name__ == '__main__':
